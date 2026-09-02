da777281410b9354a3fecd766e291f9ba58f13ff
Avichay Kagan <203189683+AvichayKagan@users.noreply.github.com>
2026-09-02 18:30:06 +0300

    Bug fix: vault operation now first check password validity and do not mutate argument in case of wrong password (return value 'false')

--- src/vault.cpp
+++ src/vault.cpp
@@ -4,45 +4,40 @@
 
 namespace vault {
 
-
-bool Vault::flush(crypto::SafeVar master_password) {
+crypto::SafeVar Vault::get_master_key(crypto::SafeVar &&master_password) {
+    crypto::SafeVar hash = std::move(master_password);
     crypto::SafeVar master_key = master_key_enc;
-    master_password.hash(salt);
+    hash.hash(salt);
 
     try {
-        master_key.decrypto(master_password.get(), true);
+        master_key.decrypto(hash.get(), true);
     }
     catch (const crypto::Error& e) {
-        if (e.code() == crypto::IncorrectPassword) return false;
+        if (e.code() == crypto::IncorrectPassword) return crypto::SafeVar();
         throw;
     }
 
-    crypto::SafeVar data = dictionary->pack();
-    data.encrypto(master_key.get());
-    master_password.memzero();
-    master_key.memzero();
-
-    disk_mang.atomic_write_file(master_key_enc, salt, data);
-
-    return true;
+    return master_key;
 }
 
 
-bool Vault::open_vault(crypto::SafeVar master_password) {
+void Vault::flush(crypto::SafeVar master_key) {
+    crypto::SafeVar data = dictionary->pack();
+    data.encrypto(master_key.get());
+    master_key.memzero();
+
+    disk_mang.atomic_write_file(master_key_enc, salt, data);
+}
+
+
+bool Vault::open_vault(crypto::SafeVar &&master_password) {
+    crypto::SafeVar master_key = get_master_key(std::move(master_password));
+    if (master_key.get() == nullptr) return false;
+
     // read disk
     crypto::SafeVar data = disk_mang.read_vault_data();
 
-    master_password.hash(salt);
-    crypto::SafeVar master_key = master_key_enc;
-    try {
-        master_key.decrypto(master_password.get(), true);
-    }
-    catch (const crypto::Error& e) {
-        if (e.code() == crypto::IncorrectPassword) return false;
-        throw;
-    }
     data.decrypto(master_key.get(), false);
-    master_password.memzero();
     master_key.memzero();
 
     dictionary = std::make_unique<Dict>(std::move(data));
@@ -52,17 +47,13 @@ bool Vault::open_vault(crypto::SafeVar master_password) {
 
 
 void Vault::init_vault(crypto::SafeVar &&master_passowrd) {
-    crypto::SafeVar password_hash;
-    crypto::SafeVar data;
-
     master_key_enc.random();
     crypto::random(salt, crypto::salt_len);
     
-    password_hash = master_passowrd;
+    crypto::SafeVar password_hash = master_passowrd;
     password_hash.hash(salt);
     
     master_key_enc.encrypto(password_hash.get());
-    password_hash.memzero();
 
     dictionary = std::make_unique<Dict>(crypto::SafeVar(0)); // inlined open_vault for the case of empty vault that bypass the read/write chicken and egg issue
 
@@ -71,16 +62,19 @@ void Vault::init_vault(crypto::SafeVar &&master_passowrd) {
 
 
 bool Vault::add_password(crypto::SafeVar &&name, crypto::SafeVar &&password, crypto::SafeVar &&master_password) {
+    crypto::SafeVar master_key;
+    if (master_password.get() != nullptr) {
+        master_key = get_master_key(std::move(master_password));
+        if (master_key.get() == nullptr) return false;
+    }
+
     if (dictionary->contains(name)) return true;
     
     auto name_it = dictionary->emplace(std::move(name), std::move(password)).first; // the change to revert in case of exception
 
-    if (master_password.get() != nullptr) {
+    if (master_key.get() != nullptr) {
         try {
-            if (!flush(std::move(master_password))) {
-                dictionary->erase(name_it);
-                return false;
-            }
+            flush(std::move(master_key));
         }
         catch (...) {
             dictionary->erase(name_it);
@@ -93,16 +87,19 @@ bool Vault::add_password(crypto::SafeVar &&name, crypto::SafeVar &&password, cry
 
 
 bool Vault::del_password(crypto::SafeVar &name, crypto::SafeVar &&master_password) {
+    crypto::SafeVar master_key;
+    if (master_password.get() != nullptr) {
+        master_key = get_master_key(std::move(master_password));
+        if (master_key.get() == nullptr) return false;
+    }
+
     if (!dictionary->contains(name)) return true;
 
     auto node = dictionary->extract(name); // the change to revert in case of exception
 
-    if (master_password.get() != nullptr) {
+    if (master_key.get() != nullptr) {
         try {
-            if (!flush(std::move(master_password))) {
-                dictionary->insert(std::move(node));
-                return false;
-            }
+            flush(std::move(master_key));
         }
         catch (...) {
             dictionary->insert(std::move(node)); // noexcept?
@@ -114,14 +111,17 @@ bool Vault::del_password(crypto::SafeVar &name, crypto::SafeVar &&master_passwor
 }
 
 bool Vault::change_password(crypto::SafeVar &name, crypto::SafeVar &&password, crypto::SafeVar &&master_password) {
+    crypto::SafeVar master_key;
+    if (master_password.get() != nullptr) {
+        master_key = get_master_key(std::move(master_password));
+        if (master_key.get() == nullptr) return false;
+    }
+
     crypto::SafeVar old_password = dictionary->change_password(name, std::move(password));
 
-    if (master_password.get() != nullptr) {
+    if (master_key.get() != nullptr) {
         try {
-            if (!flush(std::move(master_password))) {
-                dictionary->change_password(name, std::move(old_password));
-                return false;
-            }
+            flush(std::move(master_key));
         }
         catch (...) {
             dictionary->restore_password(name, std::move(old_password));
@@ -134,14 +134,17 @@ bool Vault::change_password(crypto::SafeVar &name, crypto::SafeVar &&password, c
 
 
 bool Vault::change_name(crypto::SafeVar &&name, crypto::SafeVar &new_name, crypto::SafeVar &&master_password) {
+    crypto::SafeVar master_key;
+    if (master_password.get() != nullptr) {
+        master_key = get_master_key(std::move(master_password));
+        if (master_key.get() == nullptr) return false;
+    }
+
     dictionary->change_name(name, crypto::SafeVar(new_name));
 
-    if (master_password.get() != nullptr) {
+    if (master_key.get() != nullptr) {
         try {
-            if (!flush(std::move(master_password))) {
-                dictionary->change_name(new_name, std::move(name));
-                return false;
-            }
+            flush(std::move(master_key));
         }
         catch (...) {
             dictionary->change_name(new_name, std::move(name));
@@ -154,29 +157,20 @@ bool Vault::change_name(crypto::SafeVar &&name, crypto::SafeVar &new_name, crypt
 
 
 bool Vault::change_master(crypto::SafeVar &&new_master, crypto::SafeVar &&master_password) {
-    crypto::SafeVar master_key = master_key_enc;
-    crypto::SafeVar password_hash = std::move(master_password);
-    password_hash.hash(salt);
-    try {
-        master_key.decrypto(password_hash.get(), true);
-    }
-    catch (const crypto::Error& e) {
-        if (e.code() == crypto::IncorrectPassword) return false;
-        throw;
-    }
-    
+    crypto::SafeVar master_key = get_master_key(std::move(master_password));
+    if (master_key.get() == nullptr) return false;
 
-    crypto::SafeVar new_hash = new_master;
+    // encyrpt the new master
+    crypto::SafeVar new_hash = std::move(new_master);
     new_hash.hash(salt);
-    master_key.encrypto(new_hash.get());
-    crypto::SafeVar old_master_key_enc = std::move(master_key_enc); // the point of revert
-    master_key_enc = std::move(master_key); // no except
+    crypto::SafeVar new_master_enc = master_key;
+    new_master_enc.encrypto(new_hash.get());
+
+    crypto::SafeVar old_master_key_enc = std::move(master_key_enc); // the point of revert, no except
+    master_key_enc = std::move(new_master_enc); // no except
 
     try {
-        if (!flush(std::move(new_master))) {
-            master_key_enc = std::move(old_master_key_enc);
-            return false;
-        }
+        flush(std::move(master_key));
     }
     catch (...) {
         master_key_enc = std::move(old_master_key_enc);
